d032693a24f8bda551edcd335bf6b87248672c1e
1 <1@DESKTOP-N565T0B>
2021-03-03 14:30:12 +0200

    Rebuild structure of the project

---
+++ DAL/Context/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Context
+{
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite("Data Source=YourDVVS.db");
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DAL/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public int Role { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
