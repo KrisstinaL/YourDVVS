637669318203729a792ce5639d08730d2fd96324
[Your <hksheva@gmail.com>
2021-03-16 20:45:21 +0200

    Create migration)))

---
+++ YourDVVS/DAL/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public int Role { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
