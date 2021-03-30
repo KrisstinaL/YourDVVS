68252a12f9ccca45c22b3470a0833c6dca3104e3
natalistruk <desnaa28@gmail.com>
2021-03-30 21:16:37 +0300

    Add Interfaces

--- YourDVVS/BLL/Interfaces/IAccountManagement.cs
+++ YourDVVS/BLL/Interfaces/IAccountManagement.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DAL.Entities;
 
 namespace BLL.Interfaces
 {
     interface IAccountManagement
     {
+        void AddNewUser(string lastName, string firstName, string middleName, int role, string login, string password);
+        string GetRoleName(int roleValue);
+        User GetLecturer(int id);
+        public string[] GetLecturers();
+        public int GetLecturerId(string name);
+        Student GetStudent(int id);
+        User GetUser(string username);
+        bool Verify(string username, string password);
     }
 }
